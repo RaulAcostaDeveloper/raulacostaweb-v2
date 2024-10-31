8885839603cde4b0ae7a6b7477e1d45fb02d6141
raulacosta <aacosta@better.com.mx>
2024-10-31 09:57:50 -0600

    Nuevo CV (añadido)

--- src/PagesScreens/dev/curriculum/curriculumPage.tsx
+++ src/PagesScreens/dev/curriculum/curriculumPage.tsx
@@ -7,7 +7,11 @@ export const CvPage = () => {
             <p id="curriculum">Hecha un vistazo a mi currículum profesional para saber más detalle.</p>
             <div className="contenedorCurriculums">
                 <a href="/images/cvs/1.jpg" target="_blank" className="curriculum" title="Ver mi CV">
-                    <Image src={'/images/cvs/1.jpg'} alt="cv en español" width={150} height={200} />
+                    <Image src={'/images/cvs/1.jpg'} alt="cv en inglés" width={150} height={200} />
+                    <h3>Resume in english (ATS)</h3>
+                </a>
+                <a href="/images/cvs/2.jpg" target="_blank" className="curriculum" title="Ver mi CV">
+                    <Image src={'/images/cvs/2.jpg'} alt="cv en inglés" width={150} height={200} />
                     <h3>Resume in english</h3>
                 </a>
             </div>
