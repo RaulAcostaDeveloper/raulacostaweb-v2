8fcf66abe2be2392248706fefb30e6648d137676
raulacosta <aacosta@better.com.mx>
2024-10-31 13:30:43 -0600

    Quitar el hover de la imagen de portada

--- src/app/dev/layout.tsx
+++ src/app/dev/layout.tsx
@@ -3,30 +3,32 @@ import { MenuNavegationHeader } from "@/Components/MenuNavegationHeader/MenuNave
 import { SelectorDePaginaDev } from "@/Components/SelectorDePaginaDev/SelectorDePaginaDev";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <>
-            <MenuNavegationHeader options={[
-                {
-                    title: 'Perfil',
-                    url: '/dev'
-                },
-                {
-                    title: 'Currículum',
-                    url: '/dev/curriculum'
-                },
-                {
-                    title: 'Portafolio',
-                    url: '/dev/portafolio'
-                },
-                {
-                    title: 'Certificados',
-                    url: '/dev/certificados'
-                },
-            ]} />
-            <FotoBanner imageName="1" title="Front End Dev" />
-            <SelectorDePaginaDev />
-            {children}
-            <SelectorDePaginaDev />
-        </>
-    )
+  return (
+    <>
+      <MenuNavegationHeader
+        options={[
+          {
+            title: "Perfil",
+            url: "/dev",
+          },
+          {
+            title: "Currículum",
+            url: "/dev/curriculum",
+          },
+          {
+            title: "Portafolio",
+            url: "/dev/portafolio",
+          },
+          {
+            title: "Certificados",
+            url: "/dev/certificados",
+          },
+        ]}
+      />
+      <FotoBanner imageName="1" title="Raúl Acosta Desarrollador Front End" />
+      <SelectorDePaginaDev />
+      {children}
+      <SelectorDePaginaDev />
+    </>
+  );
 }
