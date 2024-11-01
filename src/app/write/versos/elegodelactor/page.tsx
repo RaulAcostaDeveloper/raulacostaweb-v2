5345a0c6c9597ce2b3d4eec5fb71ab53f5941543
Raúl Acosta <raulacostadeveloper@outlook.com>
2024-11-01 09:54:56 -0600

    El ego del actor

--- src/app/write/versos/elegodelactor/page.tsx
+++ src/app/write/versos/elegodelactor/page.tsx
@@ -4,9 +4,17 @@ export default function WritePage() {
   return (
     <main>
       <ContenidoDesplegable title="El ego del actor">
-        <i>
-          <p></p>
-        </i>
+      <i>
+                <p>Aquel día se estremecieron ante las palabras del actor</p>
+                <p>Ya tenía preparado su discurso dantesco a sabiendas de presencia</p>
+                <p>Hasta su majestad la terrible tembló desde sus privilegios</p>
+                <p>No hizo falta la orden para que los cortados se llevaran al creído</p>
+                <p>Solo ellos ignoraban la magnitud de sus estruendos</p>
+                <p>Después de un tiempo lo trajeron a su reina</p>
+                <p>"Nadie puede alzar la voz frente a su majestad,</p>
+                <p>menos un anglosio adquirido como tú"</p>
+              </i>
+              <p>Nota: anglosio (sin lengua)</p>
       </ContenidoDesplegable>
     </main>
   );
