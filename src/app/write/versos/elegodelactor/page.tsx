07cf70f1e94dccf4ed5f98d6e2590d816aad0166
Raúl Acosta <raulacostadeveloper@outlook.com>
2024-11-01 10:00:32 -0600

    Las comillas son caracteres especiales

--- src/app/write/versos/elegodelactor/page.tsx
+++ src/app/write/versos/elegodelactor/page.tsx
@@ -11,8 +11,8 @@ export default function WritePage() {
                 <p>No hizo falta la orden para que los cortados se llevaran al creído</p>
                 <p>Solo ellos ignoraban la magnitud de sus estruendos</p>
                 <p>Después de un tiempo lo trajeron a su reina</p>
-                <p>"Nadie puede alzar la voz frente a su majestad,</p>
-                <p>menos un anglosio adquirido como tú"</p>
+                <p>&quot;Nadie puede alzar la voz frente a su majestad,</p>
+                <p>menos un anglosio adquirido como tú&quot;</p>
               </i>
               <p>Nota: anglosio (sin lengua)</p>
       </ContenidoDesplegable>
